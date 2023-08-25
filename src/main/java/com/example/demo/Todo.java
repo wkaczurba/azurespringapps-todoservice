25e742f80ec3584ae9db73131bd744ce1eba4f05
Witold Kaczurba <witold.kaczurba@gmail.com>
2023-08-25 14:45:52 +0200

    sources

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Todo {
+
+    public Todo() {
+    }
+
+    public Todo(String description, boolean done) {
+        this.description = description;
+        this.done = done;
+    }
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String description;
+
+    private boolean done;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
---
+++ src/main/java/com/example/demo/TodoController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+
+@RestController
+public class TodoController {
+
+    private final TodoRepository todoRepository;
+
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @PostConstruct
+    public void init() {
+        todoRepository.saveAll(Arrays.asList(
+                new Todo("First item", true),
+                new Todo("Second item", true),
+                new Todo("Third item", false)));
+    }
+
+    @GetMapping("/")
+    public Iterable<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
+}
